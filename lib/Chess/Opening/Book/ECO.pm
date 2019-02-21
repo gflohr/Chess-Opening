9258146de65f5d16e1e8567ecd73ac78320abb59
Guido Flohr <guido.flohr@cantanea.com>
2019-02-21 09:11:44 +0200

    ECO book

---
+++ lib/Chess/Opening/Book/ECO.pm
@@ -0,0 +1,50 @@
+#! /bin/false
+
+# Copyright (C) 2018 Guido Flohr <guido.flohr@cantanea.com>,
+# all rights reserved.
+
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What the Fuck You Want
+# to Public License, Version 2, as published by Sam Hocevar. See
+# http://www.wtfpl.net/ for more details.
+
+# Make Dist::Zilla happy.
+# ABSTRACT: Read chess opening books in polyglot format
+
+package Chess::Opening::Book::ECO;
+
+use common::sense;
+
+use base 'Chess::Opening::Book';
+
+use Fcntl qw(:seek);
+
+use Chess::Opening::Book::Polyglot::Random64;
+
+sub new {
+	my $self = '';
+
+	require Chess::Opening::ECO::Data;
+
+	bless \$self, shift;
+}
+
+sub lookupFEN {
+	my ($self, $fen) = @_;
+
+	my $positions = Chess::Opening::ECO::Data->positions;
+
+	return if !exists $positions->{$fen};
+
+	my $position = $positions->{$fen};
+	my $entry = Chess::Opening::Book::Entry->new($fen);
+
+	foreach my $move (keys %{$position->{moves}}) {
+		$entry->addMove(move => $move);
+	}
+
+	return $entry;
+}
+
+1;
